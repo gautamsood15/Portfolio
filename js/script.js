79fc71eb81ef354b95b39d4bcff7ff7a75bc2255
gautamsood15 <gauty22@gmail.com>
2019-12-12 00:06:07 +0530

    typing feature of the sub title is added in the slider section

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
 $(document).ready(function() {
 
 	$('#slides').superslides({
-
 		animation: 'fade',
 		play: 5000,
-		pagination: false 
+		pagination: false
 	});
+
+	var typed = new Typed(".typed", {
+		strings: ["Software Engineer.", "Python Developer.", "Student."],
+		typeSpeed: 70,
+		loop: true,
+		startDelay: 1000,
+		showCursor: false
+	});
+
 });

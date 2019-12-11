4e3f60f1e4454b2b74bc1f0d03c29e3d268b442a
gautamsood15 <gauty22@gmail.com>
2019-12-12 04:11:36 +0530

    owl carousel and its js and css files added + documentation for scripts.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,38 @@
 $(document).ready(function() {
 
-	$('#slides').superslides({
+	$('#slides').superslides({     /* superslide script */
 		animation: 'fade',
 		play: 5000,
 		pagination: false
 	});
 
-	var typed = new Typed(".typed", {
+
+
+	var typed = new Typed(".typed", {                    
 		strings: ["Software Engineer.", "Python Developer.", "Student."],
-		typeSpeed: 70,
-		loop: true,
+		typeSpeed: 70,               
+		loop: true,                                 /* Typed script */
 		startDelay: 1000,
 		showCursor: false
 	});
+
+
+
+	$('.owl-carousel').owlCarousel({                  /* owl carousel script */
+	    loop:true,
+	    margin:10,
+	    nav:true,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:3
+	        },
+	        1000:{
+	            items:5
+	        }
+	    }
+	})                                                               
 
 });

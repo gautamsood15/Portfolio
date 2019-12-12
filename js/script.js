a99903447a5aca2dc507938cd12abfdff17fdcc5
gautamsood15 <gauty22@gmail.com>
2019-12-12 22:52:25 +0530

    Adding chart options in script.js

--- js/script.js
+++ js/script.js
@@ -1,45 +1,49 @@
 $(document).ready(function() {
 
-	$('#slides').superslides({     /* superslide script */
+	$('#slides').superslides({
 		animation: 'fade',
 		play: 5000,
 		pagination: false
 	});
 
-
-
-	var typed = new Typed(".typed", {                    
-		strings: ["Software Engineer.", "Python Developer.", "Student."],
-		typeSpeed: 70,               
-		loop: true,                                 /* Typed script */
+	var typed = new Typed(".typed", {
+		strings: ["Software Engineer.", "Web Developer.", "Student."],
+		typeSpeed: 70,
+		loop: true,
 		startDelay: 1000,
 		showCursor: false
 	});
 
-
-
-	$('.owl-carousel').owlCarousel({                  /* owl carousel script */
+	$('.owl-carousel').owlCarousel({
 	    loop:true,
-	    margin:10,
-	    nav:true,
+	    items: 4,
 	    responsive:{
 	        0:{
 	            items:1
 	        },
-	        600:{
+	        480:{
+	            items:2
+	        },
+	        768:{
 	            items:3
 	        },
-	        1000:{
-	            items:5
+	        938:{
+	            items:4
 	        }
 	    }
 	});
 
 
 	$('.chart').easyPieChart({
-    	       
-
-
-    });                                                              
+        easing: 'easeInOut',
+        barColor: '#fff',
+        trackColor: false,
+        scaleColor: false,
+        lineWidth: 4,
+        size: 152,
+        onStep: function(from, to, percent) {
+        	$(this.el).find('.percent').text(Math.round(percent));
+        }
+    });
 
 });
